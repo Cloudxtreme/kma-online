50df03d95c07710fd8603d802adccb1dc5ee6b47
Justin <jdeanwaite@gmail.com>
2015-06-17 11:49:50 -0600

    fix to production database url using openshift's environmental vars

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = function () {
 function getMongoUrl() {
 	switch (process.env.NODE_ENV) {
 		case "production":
-			return "mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/";
+			return process.env.OPENSHIFT_MONGODB_DB_URL; //"mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/";
 		case "waite":
 		case "dorff":
 			return "mongodb://localhost/kma";
